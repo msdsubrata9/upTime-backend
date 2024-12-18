fe4532c51adc1221f2304f558eab62e216bea310
Subrata Saha <msdsubrata9@gmail.com>
2024-12-18 12:12:16 +0530

    Added Production Link

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const cors = require("cors");
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://uptime-web-nu.vercel.app"],
     credentials: true,
   })
 );
