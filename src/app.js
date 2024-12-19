71286d13dc4107f35bde5aacd9e4266095d48806
Subrata Saha <msdsubrata9@gmail.com>
2024-12-19 11:59:28 +0530

    Deployment modifications

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const cors = require("cors");
 
 app.use(
   cors({
-    origin: "https://uptime-web-45ccd.web.app",
+    origin: "http://localhost:3000",
     credentials: true,
   })
 );
@@ -19,13 +19,16 @@ app.use(cookieParser());
 app.use("/", authRouter);
 app.use("/", serviceRouter);
 
+app.get("/", (req, res) => {
+  res.send("Welcome to the uptime-backend API");
+});
+
 connectDB()
   .then(() => {
     console.log("Database connection established successfully");
-    app.listen(7777, () => {
-      console.log("Serever is successfully listening on port 7777...");
-    });
   })
   .catch((err) => {
     console.error("Database is unable to connect!!!");
   });
+
+module.exports = app;
