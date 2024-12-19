eb3708eb7ec5d8d751e42637f352c841241b6ca1
Subrata Saha <msdsubrata9@gmail.com>
2024-12-19 13:16:30 +0530

    revert back

--- src/app.js
+++ src/app.js
@@ -8,9 +8,7 @@ const cors = require("cors");
 
 app.use(
   cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    origin: "http://localhost:3000",
     credentials: true,
   })
 );
@@ -21,16 +19,14 @@ app.use(cookieParser());
 app.use("/", authRouter);
 app.use("/", serviceRouter);
 
-app.get("/", (req, res) => {
-  res.send("Welcome to the uptime-backend API");
-});
-
 connectDB()
   .then(() => {
     console.log("Database connection established successfully");
+    app.listen(7777, () => {
+      console.log("Serever is successfully listening on port 7777...");
+    });
   })
   .catch((err) => {
     console.error("Database is unable to connect!!!");
   });
-
 module.exports = app;
